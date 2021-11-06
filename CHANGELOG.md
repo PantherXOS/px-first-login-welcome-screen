74244b923efafabbb4eda7e1e35695966ab9d45f
h.nasajpour <h.nasajpour@pantherx.org>
2021-11-06 12:52:02 +0330

    Adding options to run once or force running even if it has been ran previously. #4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 
+## [0.0.3]
+### Initial Version
+
+ - Adding options to run once or force running even if it has been ran previously.
+
+
 ## [0.0.2]
 ### Initial Version
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,46 @@
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDateTime>
+#include <QDebug>
+#include <QStandardPaths>
+#include <QDir>
+#include <QCommandLineParser>
 #include "MainWindow.h"
+
+#define  FIRST_LOGIN_SCREEN_FILE_PATH       QStandardPaths::standardLocations(QStandardPaths::CacheLocation)[0]
+#define  FIRST_LOGIN_SCREEN_FILE_NAME       FIRST_LOGIN_SCREEN_FILE_PATH + "/info"
+#define  VERSION                            "0.0.3"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    QApplication::setApplicationName("px-first-login-welcome-screen");
+    QApplication::setApplicationVersion(VERSION);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("PantherX First Login Welcome Screen");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    QCommandLineOption forceOption(QStringList() << "f" << "force",
+                                    "Force running even if '~/.cache/px-first-login-welcome-screen/info' was exist");
+    parser.addOption(forceOption);
+    parser.process(app);
+
+    QFile file(FIRST_LOGIN_SCREEN_FILE_NAME);
+    if(!parser.isSet(forceOption) && file.exists()) {
+        return -1;
+    } else {
+        QDir path(FIRST_LOGIN_SCREEN_FILE_PATH);
+        if(!path.exists()){
+            path.mkpath(FIRST_LOGIN_SCREEN_FILE_PATH);
+        }
+        if (file.open(QIODevice::ReadWrite)) {
+            QTextStream stream(&file);
+            stream << "time:" << QDateTime::currentMSecsSinceEpoch() << Qt::endl;
+            stream << "version:" << VERSION << Qt::endl;
+            file.close();
+        }
+    }
     MainWindow window;
     return app.exec(); 
 }
